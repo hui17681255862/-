1cf4930f5754ddb7b62adfec64518033224318b8
dong4716138 <dong4716138@163.com>
2019-05-27 11:17:05 +0800

    从数据库news中查询前6个，然后再index.html模板中进行了测试

--- app/views/index/views.py
+++ app/views/index/views.py
@@ -2,9 +2,13 @@ from flask import render_template
 
 from app.models.models import News
 from . import index_blu
+from app import db
 
 
 @index_blu.route("/index")
 @index_blu.route("/")
 def index():
-    return render_template("index/index.html")
+    # 1. 查询点击排行的前6个数据
+    news = db.session.query(News).order_by(News.clicks.desc()).limit(6)  # 从news数据表中查询点击最高的6个数据
+    
+    return render_template("index/index.html", news=news)
